13ad3c5ded1ed5b0bc6e3e57fcef016d1df86950
wadhawansamarth <133197158+wadhawansamarth@users.noreply.github.com>
2024-10-04 22:32:30 +0530

    Add files via upload

---
+++ src/auth/Auth0ProviderWithNavigate.tsx
@@ -0,0 +1,42 @@
+import { AppState, Auth0Provider } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const Auth0ProviderWithNavigate = ({ children }: Props) => {
+  const navigate = useNavigate();
+
+  const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+  const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+  const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+  const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
+
+  if (!domain || !clientId || !redirectUri || !audience) {
+    throw new Error("unable to initialise auth");
+  }
+
+  const onRedirectCallback = (appState?: AppState) => {
+    navigate(appState?.returnTo || "/auth-callback");
+  };
+
+  return (
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      // this code added after getting refresh error only 2 lines added line 29 and 30 
+      useRefreshTokens={true}
+      cacheLocation="localstorage"
+      authorizationParams={{
+        redirect_uri: redirectUri,
+        audience,
+      }}
+      onRedirectCallback={onRedirectCallback}
+    >
+      {children}
+    </Auth0Provider>
+  );
+};
+
+export default Auth0ProviderWithNavigate;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,43 @@
+import landingImage from "../assets/landing.png";
+import appDownloadImage from "../assets/appDownload.png";
+import SearchBar, { SearchForm } from "@/components/SearchBar";
+import { useNavigate } from "react-router-dom";
+
+const HomePage = () => {
+  const navigate = useNavigate();
+
+  const handleSearchSubmit = (searchFormValues: SearchForm) => {
+    navigate({
+      pathname: `/search/${searchFormValues.searchQuery}`,
+    });
+  };
+
+  return (
+    <div className="flex flex-col gap-12">
+      <div className=" md:px-32 bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
+        <h1 className="text-5xl font-bold tracking-tight text-orange-600">
+        Feast Without the Fuss!
+        </h1>
+        <span className="text-xl">Click in for flavor!</span>
+        <SearchBar
+          placeHolder="Search by City or Town"
+          onSubmit={handleSearchSubmit}
+        />
+      </div>
+      <div className="grid md:grid-cols-2 gap-5">
+        <img src={landingImage} />
+        <div className="flex flex-col items-center justify-center gap-4 text-center">
+          <span className="font-bold text-3xl tracking-tighter">
+          Elevate your dining experience!
+          </span>
+          <span>
+          Grab the EatOn App today for lightning-fast orders and personalized picks!
+          </span>
+          <img src={appDownloadImage} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
